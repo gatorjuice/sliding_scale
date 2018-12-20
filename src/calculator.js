fe743ef071a27361a106c1c63fabf4c4db9eb5d4
James Gates <gatorjuice@gmail.com>
2018-12-19 22:09:32 -0600

    did more stuff

--- src/calculator.js
+++ src/calculator.js
@@ -6,50 +6,114 @@ class Calculator extends Component {
     super(props);
     this.state = {
       householdMembers: 1,
-      householdIncome: 0
+      householdIncome: 0.00,
+      medicadeFee: 92.99
     };
 
     this.handleHouseholdIncomeChange = this.handleHouseholdIncomeChange.bind(this);
-    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleHouseholdMembersChange = this.handleHouseholdMembersChange.bind(this);
+    this.calculatePovertyLevel = this.calculatePovertyLevel.bind(this);
   }
 
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
+  handleHouseholdMembersChange(event) {
+    const householdMembers = event.target.value;
+
+    this.setState({householdMembers});
+
+    this.calculatePovertyLevel(householdMembers);
+  }
+
+  calculatePovertyLevel(householdMembers) {
+    const povertyLevel = 12140 + ((householdMembers - 1) * 4320);
 
     this.setState({
-      [name]: value
+      povertyLevels: [
+        povertyLevel,
+        povertyLevel * 1.25,
+        povertyLevel * 1.5,
+        povertyLevel * 1.75,
+        povertyLevel * 2,
+        povertyLevel * 2.5
+      ]
     });
   }
 
-  handleHouseholdIncomeChange(event, maskedvalue, floatvalue){
-    this.setState({householdIncome: maskedvalue});
+  handleHouseholdIncomeChange(_event, householdIncome, _floatvalue){
+    this.setState({householdIncome});
   }
 
   render() {
+    const medicadeFee = this.state.medicadeFee;
+    const scale = [
+      {
+        percentOfServiceCost: 0,
+        hourlyServiceCharge: 5
+      },
+      {
+        percentOfServiceCost: 20,
+        hourlyServiceCharge: 0.2 * medicadeFee
+      },
+      {
+        percentOfServiceCost: 40,
+        hourlyServiceCharge: 0.4 * medicadeFee
+      },
+      {
+        percentOfServiceCost: 60,
+        hourlyServiceCharge: 0.6 * medicadeFee
+      },
+      {
+        percentOfServiceCost: 80,
+        hourlyServiceCharge: 0.8 * medicadeFee
+      },
+      {
+        percentOfServiceCost: 100,
+        hourlyServiceCharge: medicadeFee
+      }
+    ];
+
     return(
-      <form>
-        <label>
-          Household Members:
-          <input
-            name="householdMembers"
-            type="number"
-            value={this.state.householdMembers}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <label>
-          Household Income:
-          <CurrencyInput
-            name="householdIncome"
-            value={this.state.householdIncome}
-            prefix='$'
-            onChangeEvent={this.handleHouseholdIncomeChange}
-          />
-        </label>
-        <input type='submit' value='Calculate' />
-      </form>
+      <div>
+        <form>
+          <label>
+            Household Members:
+            <input
+              className='form-control'
+              name='householdMembers'
+              type='number'
+              value={this.state.householdMembers}
+              onChange={this.handleHouseholdMembersChange}
+            />
+          </label>
+          <br></br>
+          <label>
+            Household Income:
+            <CurrencyInput
+              className='form-control'
+              name='householdIncome'
+              precision='0'
+              value={this.state.householdIncome}
+              prefix='$'
+              onChangeEvent={this.handleHouseholdIncomeChange}
+            />
+          </label>
+          <br></br>
+          <label>
+            Medicade Fee:
+            <CurrencyInput
+              className='form-control'
+              name='medicadeFee'
+              value={this.state.medicadeFee}
+              prefix='$'
+              onChangeEvent={this.handleInputChange}
+            />
+          </label>
+          <br></br>
+        </form>
+        <p>% of Total Service Cost:</p>
+        <h3>{scale[3].percentOfServiceCost}</h3>
+        <p>Hourly Service Charge:</p>
+        <h3>{scale[3].hourlyServiceCharge}</h3>
+      </div>
     )
   }
 }
