fa7c5007b8f08e96783b90ef0fe9c75311008ca5
James Gates <gatorjuice@gmail.com>
2018-12-19 22:30:07 -0600

    Updates

---
+++ precache-manifest.5911818b8858eabc317813655599db48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "09a3f846a31aaf039ec7",
+    "url": "/sliding_scale/static/css/main.1bf73151.chunk.css"
+  },
+  {
+    "revision": "09a3f846a31aaf039ec7",
+    "url": "/sliding_scale/static/js/main.09a3f846.chunk.js"
+  },
+  {
+    "revision": "202d849ac2846718f13a",
+    "url": "/sliding_scale/static/css/1.34296182.chunk.css"
+  },
+  {
+    "revision": "202d849ac2846718f13a",
+    "url": "/sliding_scale/static/js/1.202d849a.chunk.js"
+  },
+  {
+    "revision": "e51f71072f28c815ecd8",
+    "url": "/sliding_scale/static/js/runtime~main.e51f7107.js"
+  },
+  {
+    "revision": "51ff7f85611cfb6ea04ed3fc2854a9c6",
+    "url": "/sliding_scale/index.html"
+  }
+];
